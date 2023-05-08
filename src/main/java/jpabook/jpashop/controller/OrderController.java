231d555ca66654b223d2432d238566fb38a5170f
chanyoung <rhcksdud777@naver.com>
2023-05-09 01:00:34 +0900

    주문 내역 조회 후 CANCEL 기능 추가

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -10,10 +10,7 @@ import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -51,9 +48,14 @@ public class OrderController {
         Service에서 바로 Controller로 가도록 설계해도 괜찮다.*/
         model.addAttribute("orders", orders);
 
-
         return "order/orderList";
 
 
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId){
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
     }
 }
